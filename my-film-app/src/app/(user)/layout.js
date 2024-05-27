e0cb83a62faef43c4b9a34e04f2f2b54a329b335
h8 <h.latifi03@gmail.com>
2024-05-27 18:32:22 +0330

    add react hot toast

--- my-film-app/src/app/(user)/layout.js
+++ my-film-app/src/app/(user)/layout.js
@@ -3,6 +3,8 @@ import vazirFont from "../../constants/localFonts";
 import Header from "../../components/Header";
 import Navigation from "../../components/Navigation";
 import { Footer } from '@/components/Footer';
+import  { Toaster } from 'react-hot-toast';
+
 
 
 
@@ -15,7 +17,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body  className={`${vazirFont.variable} font-sans`}>
+
           <main className="grid grid-cols-5 max-w-[1440px] mx-auto bg-slate-800">
+            <Toaster/>
             <div className="col-span-1 shadow-xl ">
               <Navigation />
             </div>
